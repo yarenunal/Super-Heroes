6370d6cfe44ceb875eff7622082e271c2153f183
yarenunal <yareunal16@gmail.com>
2024-07-23 16:33:30 +0300

    Initial Commit

---
+++ Super Heroes/Super_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_HeroesApp.swift
+//  Super Heroes
+//
+//  Created by Yaren on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Super_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
